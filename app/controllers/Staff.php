59207956158efa01dbb911d305d6716ae42af582
Ferdian Afza <farisafza201002@gmail.com>
2023-12-04 09:41:31 +0700

    tambah controller staff

---
+++ app/controllers/Staff.php
@@ -0,0 +1,57 @@
+<?php
+
+class Staff extends Controller{
+	
+	// view form create account
+	public function new(){
+		$data['judul'] = 'Buat Akun Staff';
+		$this->view('templates/header', $data);
+		$this->view('staff/new');
+		$this->view('templates/footer');
+	}
+
+	// create account
+	public function create(){
+		if($this->model('Staff_model')->createStaff($_POST) > 0){
+			Flasher::setFlash('berhasil', 'ditambahkan', 'success');
+			header('Location: '.BASEURL.'/staff/new');
+			exit;
+		} else {
+			Flasher::setFlash('gagal', 'ditambahkan', 'danger');
+			header('Location: '.BASEURL.'/staff/new');
+			exit;
+		}
+	}
+
+	// view form login
+	public function login(){
+		$data['judul'] = 'Login Staff';
+		$this->view('templates/header', $data);
+		$this->view('staff/login');
+		$this->view('templates/footer');
+	}
+
+	// validate login
+	public function session(){
+	    $userData = $this->model('Staff_model')->createSession($_POST);
+
+	    if ($userData) {
+	        session_start();
+	        $_SESSION["login"] = true;
+	        $_SESSION["username"] = $userData["username"];
+	        $_SESSION["email"] = $userData["email"];
+	        $_SESSION["id"] = $userData["id"];
+	        Flasher::setFlash('berhasil', 'login', 'success');
+	        header('Location: '.BASEURL.'/dashboard/index');
+	        exit;
+	    } else {
+	        Flasher::setFlash('gagal', 'login', 'danger');
+	        header('Location: '.BASEURL.'/staff/login');
+	        exit;
+	    }
+	}
+
+
+
+	
+}
