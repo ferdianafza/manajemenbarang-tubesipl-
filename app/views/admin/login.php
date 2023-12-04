dca1a49a4f5f9ae8a523471b5aefee408eecfd58
AHMAD-BADAR\HP <ahmadbadar145@gmail.com>
2023-12-04 10:35:06 +0700

    Tambah view login admin

---
+++ app/views/admin/login.php
@@ -0,0 +1,21 @@
+<div class="container mt-5" >
+	<div class="row">
+		<div class="col-lg-6">
+			<?php Flasher::flash();?>
+		</div>
+	</div>
+	<form action="<?= BASEURL; ?>/admin/session" method="post">
+		<div class="mb-3">
+		  <label for="email" class="form-label">Email </label>
+		  <input type="email" class="form-control" id="email" name="email" placeholder="email" required="">
+		</div>
+		<div class="mb-3">
+		  <label for="password" class="form-label">Password</label>
+		  <input type="password" class="form-control" id="password" name="password" placeholder="username" required="">
+		</div>
+		<div class="mb-3">
+		  <button class="btn btn-primary" type="submit">Log in</button>
+		</div>
+	</form>
+	<a href="<?= BASEURL;?>/admin/new">Create Acount</a>
+</div>
