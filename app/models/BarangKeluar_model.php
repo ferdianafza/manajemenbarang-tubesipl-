228644bddfa139991ed20f54593fe32fbc9336fc
Ferdian Afza <farisafza201002@gmail.com>
2023-12-06 13:51:21 +0700

    tambah fungsi getAllBarangKeluar, getAllBarangKeluarWithBarang minusBarang($data) pada model barang keluar

---
+++ app/models/BarangKeluar_model.php
@@ -0,0 +1,39 @@
+<?php
+
+class BarangKeluar_model{
+	private $table = 'barang_keluar';
+	private $db;
+
+	public function __construct(){
+		$this->db = new Database;
+	}
+
+	public function getAllBarangKeluar(){
+		$this->db->query('SELECT * FROM '.$this->table);
+		return $this->db->resultSet();
+	}
+
+	 public function getAllBarangKeluarWithBarang() {
+         $query = "SELECT bk.id_barang_keluar, a.username as admin_username, bk.barang_id, b.nama_barang, bk.jumlah, bk.waktu
+                  FROM barang_keluar bk
+                  JOIN admin a ON bk.admin_id = a.id
+                  JOIN barang b ON bk.barang_id = b.id";
+
+        $this->db->query($query);
+        return $this->db->resultSet();
+    }
+	
+	public function minusBarang($data){
+
+		$date = date('Y-m-d H:i:s');
+		$query = "INSERT INTO barang_keluar VALUES('',:admin_id ,:barang_id,:jumlah,:waktu)";
+		$this->db->query($query);
+		$this->db->bind(':admin_id', $data['admin_id']);
+		$this->db->bind(':barang_id', $data['barang_id']);
+		$this->db->bind(':jumlah', $data['jumlah']);
+		$this->db->bind(':waktu', $date);
+		$this->db->execute();
+		return $this->db->rowCount();
+	}
+	
+}
