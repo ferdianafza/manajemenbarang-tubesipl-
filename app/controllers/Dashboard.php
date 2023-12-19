ec50941108bc6d0cb93780c593024e07c0475c6b
Ferdian Afza <farisafza201002@gmail.com>
2023-12-19 12:22:44 +0700

    menambah global variabel untuk ambil data barang yang kosong,total barang, total barang masuk, total barang keluar,dan searching data barang

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -3,7 +3,18 @@ class Dashboard extends Controller {
     public function index() {
         $dashboardModel = $this->model('Dashboard_model');
         $data = $dashboardModel->checkLoginStatus();
-
+        $data['getCountBarangEmpty'] = $this->model('Barang_model')->getCountBarangEmpty();
+        $data['getCountBarang'] = $this->model('Barang_model')->getCountBarang();
+        $data['getCountBarangMasuk'] = $this->model('BarangMasuk_model')->getCountBarangMasuk();
+        $data['getCountBarangKeluar'] = $this->model('BarangKeluar_model')->getCountBarangKeluar();
+        $data['barang'] = $this->model('Barang_model')->getAllBarang();
+        $data['barang_masuk'] = $this->model('BarangMasuk_model')->getAllBarangMasukWithBarang();
+        $data['barang_keluar'] = $this->model('BarangKeluar_model')->getAllBarangKeluarWithBarang();
+        if (isset($_POST['search_query'])) {
+            $searchQuery = $_POST['search_query'];
+            $data['barang'] = $this->model('Barang_model')->searchData($searchQuery);
+        } else {
+        }
         $this->view('templates/header', $data);
         $this->view('dashboard/index', $data);
         $this->view('templates/footer');
