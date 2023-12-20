f69ebb268c07b970cc81ee20b4d2d4bf275829e8
Ferdian Afza <farisafza201002@gmail.com>
2023-12-20 18:44:55 +0700

    buat header dan footer untuk login template

---
+++ app/views/logintemplates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Halaman <?php echo $data['judul'];?></title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet"  href="<?= BASEURL;?>/css/bootstrap.css">
+  <link rel="stylesheet"  href="<?= BASEURL;?>/css/nucleo-icons.css">
+  <link rel="stylesheet"  href="<?= BASEURL;?>/css/nucleo-svg.css">
+  <link rel="stylesheet"  href="<?= BASEURL;?>/css/nucleo-svg.css">
+  <link rel="stylesheet"  href="<?= BASEURL;?>/css/soft-ui-dashboard.css?v=1.0.7">
+</head>
+<body>
