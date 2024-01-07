d7db27cd0330c01c1475341e944e346745a64d8d
Ferdian Afza <farisafza201002@gmail.com>
2024-01-07 19:26:35 +0700

    validation session

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -21,7 +21,28 @@ class Dashboard extends Controller {
     }
 
     public function destroySession() {
+        session_start();
+        $_SESSION = [];
+        session_unset();
+        session_destroy();
+
+        unset($_SESSION["loginstaff"]);
+        unset($_SESSION["usernamestaff"]);
+        unset($_SESSION["emailstaff"]);
+        unset($_SESSION["idstaff"]);
+
+        header('Location: '.BASEURL.'/staff/login');
+        exit;
+    }
+
+    public function suratjalan(){
         $dashboardModel = $this->model('Dashboard_model');
-        $dashboardModel->destroySession();
+        $data = $dashboardModel->checkLoginStatus();
+        $data['judul'] = 'Surat Jalan';
+        $data['barang'] = $this->model('Barang_model')->getAllBarang();
+        $data['suratjalan'] = $this->model('Suratjalan_model')->getAllSuratJalanWithStaffAndBarangName();
+        $this->view('templates/header', $data);
+        $this->view('dashboard/suratjalan', $data);
+        $this->view('templates/footer');
     }
 }
--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -1,36 +1,167 @@
-<div class="container mt-3">
-	<div class="row">
-		<div class="col-lg-6">
+  <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
+      <div class="container-fluid py-1 px-3">
+        <nav aria-label="breadcrumb">
+          <h6 class="font-weight-bolder mb-0">Dashboard</h6>
+        </nav>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+          <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+            <div class="input-group">
+            </div>
+          </div>
+          <ul class="navbar-nav  justify-content-end">
+            <li class="nav-item d-flex align-items-center">
+              <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
+                <i class="fa fa-user me-sm-1"></i>
+                <span class="d-sm-inline d-none"><?= $data['usernameadmin']; ?></span>
+              </a>
+            </li>
+            <li class="nav-item pe-2 d-flex align-items-center">
+              <a href="<?= BASEURL;?>/admin/destroySession" class="nav-link text-body p-0" >
+                |logout
+              </a>
+              <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="row">
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                  <div class="numbers">
+                    <p class="text-sm mb-0 text-capitalize font-weight-bold">TOTAL BARANG</p>
+                    <h5 class="font-weight-bolder mb-0">
+                      <?= $data['getCountBarang'];?>
+                    </h5>
+                  </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                    <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                  <div class="numbers">
+                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Barang Masuk</p>
+                    <h5 class="font-weight-bolder mb-0">
+                      <?= $data['getCountBarangMasuk'];?>
+                    </h5>
+                  </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                    <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                  <div class="numbers">
+                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Barang Keluar</p>
+                    <h5 class="font-weight-bolder mb-0">
+                      <?= $data['getCountBarangKeluar'];?>
+                    </h5>
+                  </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                    <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6">
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                  <div class="numbers">
+                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Barang Kosong</p>
+                    <h5 class="font-weight-bolder mb-0">
+                      <?= $data['getCountBarangEmpty'];?>
+                    </h5>
+                  </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                    <i class="ni ni-cart text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+	  <div class="container mt-3">
+	    <div class="row">
+		  <div class="col-lg-6">
 			<?php Flasher::flash();?>
+		  </div>
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-lg-6">
-			<button type="button" class="btn btn-primary tambahData" data-bs-toggle="modal" data-bs-target="#formModal">
-  				Buat Data Barang
-			</button>
-			<br/>
-			<h3>Data Barang</h3>
-				<table class="table table-striped table-hover table-bordered" >
-					<tr>
-						<thead>
-							<th scope="col">Id</th>
-							<th scope="col">Nama Barang</th>
-							<th scope="col">Stok</th>
-						</thead>
-					</tr>
-					<?php foreach ($data['barang'] as $barang ) : ?>
-				  	<tr>
-				  		<td><?= $barang['id']; ?></td>
-				  		<td><?= $barang['nama_barang']; ?></td>
-				  		<td><?= $barang['stok']; ?></td>
-				  	</tr>
-					<?php endforeach; ?>
-				</table>
+	   <div class="row">
+		<div class="col-lg-14 mb-lg-0 mb-4">
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="">
+                  <div class="d-flex flex-column h-100">
+                    <h4>DATA BARANG</h4>
+					<button type="button" class="btn btn-primary tambahData" data-bs-toggle="modal" data-bs-target="#formModal">
+		  				Buat Data Barang
+					</button>
+					<table class="table table-striped table-hover table-bordered" >
+						<tr>
+							<thead>
+								<th scope="col">Id</th>
+								<th scope="col">Nama Barang</th>
+								<th scope="col">Stok</th>
+							</thead>
+						</tr>
+						<?php foreach ($data['barang'] as $barang ) : ?>
+					  	<tr>
+					  		<td><?= $barang['id']; ?></td>
+					  		<td><?= $barang['nama_barang']; ?></td>
+					  		<td><?= $barang['stok']; ?></td>
+					  	</tr>
+						<?php endforeach; ?>
+					</table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+		<div class="col-lg-14 mt-4">
+          <div class="card h-100 p-3">
+            <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100">
+              <span class="mask "></span>
+              <div class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3">
+              	<h4>DATA BARANG MASUK</h4>
 				<button type="button" class="btn btn-primary tambahDataBarangMasuk" data-bs-toggle="modal" data-bs-target="#formModalTambahBarangMasuk">
 	  				Tambah Barang Masuk
 				</button>
-				<table class="table table-striped table-hover table-bordered">
+                <table class="table table-striped table-hover table-bordered">
 				    <thead>
 				        <tr>
 				            <th scope="col">Id Barang Masuk</th>
@@ -52,10 +183,20 @@
 				        <?php endforeach; ?>
 				    </tbody>
 				</table>
+              </div>
+            </div>
+          </div>
+        </div>
+		<div class="col-lg-14 mt-4">
+          <div class="card h-100 p-3">
+            <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100">
+              <span class="mask "></span>
+              <div class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3">
+              	<h4>DATA BARANG KELUAR</h4>
 				<button type="button" class="btn btn-primary tambahDataBarangMasuk" data-bs-toggle="modal" data-bs-target="#formModalTambahBarangKeluar">
 	  				Tambah Barang Keluar
 				</button>
-				<table class="table table-striped table-hover table-bordered">
+                <table class="table table-striped table-hover table-bordered">
 				    <thead>
 				        <tr>
 				            <th scope="col">Id Barang Keluar</th>
@@ -77,6 +218,10 @@
 				        <?php endforeach; ?>
 				    </tbody>
 				</table>
+              </div>
+            </div>
+          </div>
+        </div>
 
 		</div>
 	</div>
@@ -87,7 +232,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="judulModal">Tambah Data Barang</h1>
+        <h1 class="modal-title fs-5" id="judulModal">Buat Data Barang</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
@@ -106,7 +251,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Tambah Data</button>
+        <button type="submit" class="btn btn-primary">Buat Data</button>
     	</form>
       </div>
     </div>
@@ -123,7 +268,7 @@
       </div>
       <div class="modal-body">
         <form action="<?= BASEURL;?>/barangMasuk/create" method="post">
-        	<input type="hidden" name="admin_id" id="admin_id" value="<?= $data['id'];?>" >
+        	<input type="hidden" name="admin_id" id="admin_id" value="<?= $data['idadmin'];?>" >
         	<div class="form-group">
 				<label for="barang_id">Pilih Barang</label>
 				<select class="form-control" id="barang_id" name="barang_id">
@@ -159,7 +304,7 @@
       </div>
       <div class="modal-body">
         <form action="<?= BASEURL;?>/barangKeluar/create" method="post">
-        	<input type="hidden" name="admin_id" id="admin_id" value="<?= $data['id'];?>" >
+        	<input type="hidden" name="admin_id" id="admin_id" value="<?= $data['idadmin'];?>" >
         	<div class="form-group">
 				<label for="barang_id">Pilih Barang</label>
 				<select class="form-control" id="barang_id" name="barang_id">
@@ -186,3 +331,4 @@
   </div>
 </div>
 <a href="<?= BASEURL;?>/admin/destroySession">Log out</a>
+</main>
