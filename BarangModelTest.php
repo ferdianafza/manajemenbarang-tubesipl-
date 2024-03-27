3f517d858e5455cf2784d5d29b0af49ceab4cf6f
Ferdian Afza <farisafza201002@gmail.com>
2024-03-27 13:41:06 +0700

    tambah test testCreateSuratJalan, testAddBarangKeluar

--- BarangModelTest.php
+++ BarangModelTest.php
@@ -4,12 +4,16 @@ use PHPUnit\Framework\TestCase;
 
 require_once 'app/models/Barang_model.php';
 require_once 'app/models/BarangMasuk_model.php';
+require_once 'app/models/BarangKeluar_model.php';
+require_once 'app/models/Suratjalan_model.php';
+require_once 'app/models/Barangsuratjalan_model.php';
 require_once 'app/core/Database.php';
+
 
 class BarangModelTest extends TestCase {
     public function testCreateBarang() {
         $barangModel = new Barang_model();
-        $data = ['nama_barang' => 'testbarulagi', 'stok' => 5];
+        $data = ['nama_barang' => 'testbarulagi3', 'stok' => 5];
         $existingData = $barangModel->searchData($data['nama_barang']);
         if(empty($existingData)) {
             $result = $barangModel->addBarang($data);
@@ -38,6 +42,65 @@ class BarangModelTest extends TestCase {
         } else {
             $this->fail('Gagal Menambahkan Data Barang Masuk.');
         }
+    }
+
+    public function testAddBarangKeluar(){
+        $barangKeluar = new BarangKeluar_model();
+        $barangModel = new Barang_model();
+        $data = ['admin_id' => 9, 'barang_id' => 37, 'jumlah' => 3];
+        $result = $barangKeluar->minusBarang($data);
+        if ($result > 0) {
+            $dataBarang = [
+                'barang_id' => $data['barang_id'],
+                'jumlah' => $data['jumlah']
+            ];
+            $updateResult = $barangModel->updateMinusStok($dataBarang);
+            if ($updateResult > 0) {
+                $this->assertTrue(true, 'Barang Keluar berhasil ditambahkan dan stok barang berhasil diperbarui.');
+            } else {
+                $this->fail('gagal update stok');
+            }
+        } else {
+            $this->fail('Gagal Menambahkan Data Barang Keluar.');
+        }
+    }
+
+    public function testCreateSuratJalan(){
+        $barangModel = new Barang_model();
+        $barangKeluar = new BarangKeluar_model();
+        $barangIds = [37, 36];
+        $jumlahs = [2,1];
+
+        foreach ($barangIds as $index => $barangId) {
+            $stok = $barangModel->getCurrentStock($barangId);
+            $jumlah = $jumlahs[$index];
+            if ($jumlah > $stok || $stok <= 0 || $jumlah <= 0) {
+                $this->fail('Gagal Buat Surat Jalan, Jumlah tidak valid untuk barang yang dipilih');
+            }
+        }
+        $dataBarangIdsAndJumlahs = [
+            'barang_id' => $barangIds,
+            'jumlah' => $jumlahs,
+            'penerima' => 'PT ASAHI',
+            'tujuan' => 'Bandung',
+            'staff_id' => 5
+        ];
+        $suratJalan = new Suratjalan_model();
+        $result = $suratJalan->createSuratJalan($dataBarangIdsAndJumlahs);
+        if ($result > 0) {
+            $kodeSuratJalan = $suratJalan->getLastKodeSuratJalan();
+            if($barangModel->updateMinusStokAfterCreateSuratJalan($kodeSuratJalan['kode_surat_jalan'], $dataBarangIdsAndJumlahs['barang_id'], $dataBarangIdsAndJumlahs['jumlah']) > 0){
+                if($barangKeluar->minusBarangAfterCreateSuratJalan($kodeSuratJalan['kode_surat_jalan'], $dataBarangIdsAndJumlahs['barang_id'], $dataBarangIdsAndJumlahs['jumlah']) > 0){
+                    $kodeSuratJalan = $suratJalan->getLastKodeSuratJalan();
+                    $barangsuratjalan = new Barangsuratjalan_model();
+                    $barangsuratjalan->addBarangSuratjalan($kodeSuratJalan['kode_surat_jalan'], $dataBarangIdsAndJumlahs['barang_id'], $dataBarangIdsAndJumlahs['jumlah']);
+                    $this->assertTrue(true, 'Berhasil Membuat Surat Jalan');
+                }
+            }
+        } else {
+            $this->fail('Gagal Membuat Surat Jalan.');
+        }
+
     }
     
 }
